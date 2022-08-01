c2e8cb2b23e08059fadf37fc726df1ef308fa682
Hayat Salawdeh <35167530+hayatzsala@users.noreply.github.com>
2022-08-01 16:22:43 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// step 1: change the text inside h1 into "Hello there!"
+
+
+// step 2: add the class "small-txt" to all h2 elements
+
+
+// step 3: grab the container div and append a form with 1 text input and 1 submit input to it using innerHTML
+
+
+// step 4: change the style of the added text input to have 100% width;
+
+
+// step 5: create an array with the team members names in it
+
+
+
+// step 6: append a footer element to the body with a signature that says "Implement with love by: " and put each item in the array in here separated by a comma
